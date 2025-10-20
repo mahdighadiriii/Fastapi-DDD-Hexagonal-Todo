3e05a1313117617b4a4cf98a96aa3e3ac2dcf056
Mahdi <mahdighadiriafzal@gmail.com>
2025-10-20 21:14:51 +0330

    Update todo_events.py

--- src/domain/events/todo_events.py
+++ src/domain/events/todo_events.py
@@ -1,23 +1,23 @@
 from dataclasses import dataclass
 
-from ..value_objects.todo_id import TodoId
 from .base import DomainEvent
+from ..value_objects.todo_id import TodoId
 
 
-@dataclass
+@dataclass(kw_only=True)
 class TodoCreated(DomainEvent):
     todo_id: TodoId
     title: str
     user_id: str
 
 
-@dataclass
+@dataclass(kw_only=True)
 class TodoCompleted(DomainEvent):
     todo_id: TodoId
     user_id: str
 
 
-@dataclass
+@dataclass(kw_only=True)
 class TodoDeleted(DomainEvent):
     todo_id: TodoId
     user_id: str
