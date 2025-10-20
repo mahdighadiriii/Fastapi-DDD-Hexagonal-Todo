b83e723b0f040a6072e444b4ae089cf744349f6e
Mahdi <mahdighadiriafzal@gmail.com>
2025-10-20 21:15:09 +0330

    Create middleware.py

---
+++ src/infrastructure/api/middleware.py
@@ -0,0 +1,13 @@
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        print(f"Request: {request.method} {request.url}")
+
+        response = await call_next(request)
+
+        print(f"Response status: {response.status_code}")
+
+        return response
